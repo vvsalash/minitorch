33bde2594200971fa42a16acabe76322e1a1f7d7
vvsalash <vova.salash609@gmail.com>
2025-09-19 20:27:24 +0300

    Solve Task 1.5

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -110,7 +110,7 @@ class ScalarTrain:
 
 if __name__ == "__main__":
     PTS = 50
-    HIDDEN = 2
+    HIDDEN = 10
     RATE = 0.5
-    data = minitorch.datasets["Simple"](PTS)
+    data = minitorch.datasets["Xor"](PTS)
     ScalarTrain(HIDDEN).train(data, RATE)
